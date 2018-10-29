c9ace64d2fa9e80310e79c13b48937b1ad070e6e
ivan4000 <sxln1@mail.ru>
2018-10-30 00:17:55 +0300

    JL43assertions final commit

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,9 +2,12 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-
+        new Main().someMethod(-5);
     }
-    private void someMethod(int a){
 
+    private void someMethod(int a) {
+        assert (a > 0);
+        // do something with a
+        System.out.println(a);
     }
 }
